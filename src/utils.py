ebe7659e2f4385caebfcb32eea1ec009be61c080
nirmalamondal <nirmalamondal2002@gmail.com>
2024-07-06 14:24:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -22,3 +23,20 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
+
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for model_name,model in models.items():
+            
+             model.fit(X_train,y_train)
+             y_train_pred = model.predict(X_train)
+             y_test_pred = model.predict(X_test)
+             train_model_score = r2_score(y_train,y_train_pred)
+             test_model_score = r2_score(y_test,y_test_pred)
+             report[model_name] = test_model_score
+
+             return report
+
+    except Exception as e:
+            raise CustomException(e,sys)    
